38467d65728f5b782fac775ec0335665cc079536
Filippo Vicentini <filippovicentini@gmail.com>
2020-07-22 11:23:16 +0200

    tests

---
+++ tests/test_mpi4jax.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""
+Run with
+
+$ mpirun -n <nproc> python test.py
+"""
+
+import jax
+import jax.numpy as np
+
+from mpi4py import MPI
+
+print(MPI.get_vendor())
+
+rank = MPI.COMM_WORLD.Get_rank()
+size = MPI.COMM_WORLD.Get_size()
+
+print("MPI rank = ", rank)
+print("MPI size = ", size)
+
+from mpi4jax import Allreduce
+
+
+def test_allreduce():
+    arr = np.ones((3, 2))
+    _arr = arr.copy()
+
+    res = Allreduce(arr, op=MPI.SUM)
+    assert np.array_equal(res, arr * size)
+    assert np.array_equal(_arr, arr)
+
+    res = jax.jit(lambda x: Allreduce(x, op=MPI.SUM))(arr)
+    assert np.array_equal(res, arr * size)
+    assert np.array_equal(_arr, arr)
+
+    res = jax.vmap(lambda x: Allreduce(x, op=MPI.SUM), in_axes=0)(arr)
+    assert np.array_equal(res, arr * size)
+    assert np.array_equal(_arr, arr)
+
+    res = jax.jit(jax.vmap(lambda x: Allreduce(x, op=MPI.SUM), in_axes=0))(arr)
+    assert np.array_equal(res, arr * size)
+    assert np.array_equal(_arr, arr)
+
+    res, grad = jax.value_and_grad(lambda x: Allreduce(x, op=MPI.SUM).sum())(arr)
+    assert np.array_equal(res, arr.sum() * size)
+    assert np.array_equal(grad, np.ones(arr.shape))
+    assert np.array_equal(_arr, arr)
+
+    res, grad = jax.jit(jax.value_and_grad(lambda x: Allreduce(x, op=MPI.SUM).sum()))(
+        arr
+    )
+    assert np.array_equal(res, arr.sum() * size)
+    assert np.array_equal(grad, np.ones(arr.shape))
+    assert np.array_equal(_arr, arr)
