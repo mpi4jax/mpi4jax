b4402f8784f5c3a134dcb95666a4b8ea76882cda
Dion Häfner <dion.haefner@nbi.ku.dk>
2020-09-21 16:54:27 +0200

    remove isort

--- mpi4jax/__init__.py
+++ mpi4jax/__init__.py
@@ -1,9 +1,5 @@
 # make sure to import mpi4py first
 # this calls MPI_Init and registers mpi4py's atexit handler
-# at exit, we wait for all pending operations to finish
-# this prevents deadlocks (see mpi4jax#22)
-import atexit
-
 from mpi4py import MPI
 
 # this registers our custom XLA functions
@@ -16,6 +12,10 @@ from .collective_ops.send import Send
 from .collective_ops.sendrecv import Sendrecv
 from .flush import flush
 from .warn import disable_omnistaging_warning
+
+# at exit, we wait for all pending operations to finish
+# this prevents deadlocks (see mpi4jax#22)
+import atexit
 
 atexit.register(flush)
 
