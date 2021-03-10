35f1283e47c125d798dcfa0589164e0cd7cd635a
Filippo Vicentini <filippovicentini@gmail.com>
2021-03-10 10:34:03 +0100

    flush gpu queue (#52)
    * flush
    
    * implement suggestions

--- mpi4jax/flush.py
+++ mpi4jax/flush.py
@@ -4,6 +4,15 @@ import jax
 def flush():
     """Wait for all pending XLA operations"""
     # as suggested in jax#4335
-    for device in jax.devices("cpu"):
+
+    devices = jax.devices("cpu")
+
+    # jax.devices("gpu") throws if no gpu available.
+    try:
+        devices.extend(jax.devices("gpu"))
+    except RuntimeError:
+        pass
+
+    for device in devices:
         noop = jax.device_put(0, device=device) + 0
         noop.block_until_ready()
