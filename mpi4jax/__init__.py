1896705b89918c136c64bffe9dddfb69fbc62db8
Filippo Vicentini <filippovicentini@gmail.com>
2021-03-10 11:46:50 +0100

    add deprecations

--- mpi4jax/__init__.py
+++ mpi4jax/__init__.py
@@ -2,6 +2,8 @@ from ._version import version as __version__  # noqa: F401
 
 from ._src import allreduce, recv, send, sendrecv, bcast, disable_omnistaging_warning
 
+from ._deprecations import Allreduce, Recv, Send, Sendrecv, Bcast
+
 __all__ = [
     "allreduce",
     "send",
@@ -9,4 +11,9 @@ __all__ = [
     "sendrecv",
     "bcast",
     "disable_omnistaging_warning",
+    "Allreduce",
+    "Recv",
+    "Send",
+    "Sendrecv",
+    "Bcast",
 ]
