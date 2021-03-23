a50dbed839283102a342c42cf3996ec7a0a5ed31
Dion Häfner <dion.haefner@nbi.ku.dk>
2021-03-23 12:46:55 +0100

    satisfy harder

--- mpi4jax/_src/comm.py
+++ mpi4jax/_src/comm.py
@@ -1,4 +1,3 @@
-
 _default_comm = None
 
 
@@ -6,6 +5,7 @@ def get_default_comm():
     global _default_comm
     if _default_comm is None:
         from mpi4py import MPI
+
         _default_comm = MPI.COMM_WORLD.Clone()
 
     return _default_comm
