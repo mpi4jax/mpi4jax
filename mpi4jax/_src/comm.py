952b0b5fb5fc6449e8b6d1c04de979502662335c
Dion Häfner <dion.haefner@nbi.ku.dk>
2021-03-23 12:45:45 +0100

    Satisfy black

--- mpi4jax/_src/comm.py
+++ mpi4jax/_src/comm.py
@@ -6,7 +6,6 @@ def get_default_comm():
     global _default_comm
     if _default_comm is None:
         from mpi4py import MPI
-
         _default_comm = MPI.COMM_WORLD.Clone()
 
     return _default_comm
