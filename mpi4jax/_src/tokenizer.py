b0e3a0d7f04a824e1fc99c2c88816ada391f4609
Thenerdstation <chaserileyroberts@gmail.com>
2022-02-01 21:52:46 -0600

    Supported nested jitts

--- mpi4jax/_src/tokenizer.py
+++ mpi4jax/_src/tokenizer.py
@@ -1,11 +1,11 @@
 import jax
 from jax._src.util import safe_map
-
+from jax import linear_util as lu
 token_override_registry = {}
 
 def _override_tokens(jaxpr, consts, token, *args):
     if token is None: # Create a new token if one is not passed.
-        token = jax.core.create_token()
+        token = jax.lax.create_token()
     def read(v):
         if type(v) is jax.core.Literal:
             return v.val
@@ -34,22 +34,27 @@ def _override_tokens(jaxpr, consts, token, *args):
             # not part of our communication protocol. 
             # This code is mostly taken form jax.core.eval_jaxpr
             subfuns, bind_params = eqn.primitive.get_bind_params(eqn.params)
+            subfuns = safe_map(lambda x: lu.wrap_init(_auto_tokenize(x.call_wrapped)), subfuns)
             ans = eqn.primitive.bind(
                 *subfuns, *safe_map(read, eqn.invars), **bind_params)
             if eqn.primitive.multiple_results:
                 safe_map(write, eqn.outvars, ans)
             else:
                 write(eqn.outvars[0], ans)
-    res = safe_map(read, jaxpr.outvars)
-    if len(res) == 1:
-        return res[0]
-    return res
+    return safe_map(read, jaxpr.outvars)
 
-def auto_tokenize(f, token=None):
-    if token is None:
-        token = jax.lax.create_token()
+def _auto_tokenize(f, token=None):
     def wrapper(*args, **kwargs):
         jaxpr = jax.make_jaxpr(f)(*args, **kwargs)
-        return _override_tokens(jaxpr.jaxpr, jaxpr.consts, token, *args, **kwargs)
+        res = _override_tokens(jaxpr.jaxpr, jaxpr.consts, token, *args, **kwargs)
+        return res
 
+    return wrapper
+
+def auto_tokenize(f, token=None):
+    def wrapper(*args, **kwargs):
+        res = _auto_tokenize(f)(*args, **kwargs)
+        if len(res) == 1:
+            return res[0]
+        return tuple(res)
     return wrapper
--- tests/test_auto_tokenize.py
+++ tests/test_auto_tokenize.py
@@ -3,6 +3,7 @@ from mpi4py import MPI
 import jax
 import jax.numpy as jnp
 import numpy as np
+from functools import partial
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -18,5 +19,21 @@ def test_allreduce():
         res, _ = allreduce(res, op=MPI.SUM)
         return res
     res = auto_tokenize(f)(arr)
-    np.testing.assert_allclose(res, arr * size * 2)
+    np.testing.assert_allclose(res, arr * size ** 2)
 
+def test_nested_jits():
+    from mpi4jax import allreduce, auto_tokenize
+
+    arr = jnp.ones((3, 2))
+    @jax.jit
+    def f(a):
+        res, _ = allreduce(a, op=MPI.SUM)
+        @jax.jit
+        def g(b):
+            res, _ = allreduce(b, op=MPI.SUM)
+            res, _ = allreduce(res, op=MPI.SUM)
+            return res
+        return g(res)
+
+    res = auto_tokenize(f)(arr)
+    np.testing.assert_allclose(res, arr * size ** 3)
