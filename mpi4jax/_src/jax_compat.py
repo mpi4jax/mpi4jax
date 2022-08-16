fad9d6de684e8899bdf8b5b2440cb9ce0840a72b
Filippo Vicentini <filippovicentini@gmail.com>
2022-08-16 13:41:23 +0200

    allow effects for MPIEffect.

--- mpi4jax/_src/jax_compat.py
+++ mpi4jax/_src/jax_compat.py
@@ -19,6 +19,62 @@ def versiontuple(verstr):
     return tuple(int(v) for v in verstr.split("."))[:3]
 
 
+jax_version = versiontuple(jax.__version__)
+
+
+if jax_version >= versiontuple("0.3.15"):
+    # abstract eval needs to return effects
+    # see https://github.com/google/jax/issues/11620
+    from functools import wraps
+    from jax.interpreters import mlir
+    from jax._src.lax import control_flow as lcf
+
+    class MPIEffect:
+        def __hash__(self):
+            # enforce a constant (known) hash
+            return hash("I love mpi4jax")
+
+    effect = MPIEffect()
+    mlir.lowerable_effects.add(effect)
+    lcf.allowed_effects.add(effect)
+
+    # Effects must be added to the allow_effects list in order to work within
+    # custom_vjp. See google/jax#11916
+    if jax_version >= versiontuple("0.3.17"):
+        # since this is very internal, try not to break mpi4jax if they move the list.
+        has_allowed_effects_list = hasattr(jax._src, "custom_derivatives") and hasattr(
+            jax._src.custom_derivatives, "allowed_effects"
+        )
+        if has_allowed_effects_list:
+            jax._src.custom_derivatives.allowed_effects.add(effect)
+        else:
+            warnings.warn(
+                "\n`jax._src.custom_derivatives.allowed_effects` does not exist in this jax version, "
+                "so MPI operations might not work from within functions with custom vjp/jvp rules.\n\n"
+                "Try upgrading mpi4jax via\n\n"
+                "      $ pip install -U mpi4jax\n\n"
+                "and open an issue on the mpi4jax repository if the issue persists."
+            )
+
+    def register_abstract_eval(primitive, func):
+        """Injects an effect object into func and registers it via `primitive.def_effectful_abstract_eval`.
+
+        (as required by JAX>=0.3.15 to ensure primitives are staged out)
+        """
+
+        @wraps(func)
+        def effects_wrapper(*args, **kwargs):
+            return func(*args, **kwargs), {effect}
+
+        primitive.def_effectful_abstract_eval(effects_wrapper)
+
+else:
+    # TODO: drop this path when we require jax>=0.3.15
+
+    def register_abstract_eval(primitive, func):
+        primitive.def_abstract_eval(func)
+
+
 def check_jax_version():
     here = os.path.dirname(__file__)
     with open(os.path.join(here, "_latest_jax_version.txt")) as f:
