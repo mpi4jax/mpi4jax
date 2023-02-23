6d8087ac3735f0445ffb192378de1a0972714fda
Dion Häfner <dion.haefner@simulation.science>
2023-02-23 09:57:13 +0100

    mark experimental module as broken for recent jax

--- mpi4jax/experimental/__init__.py
+++ mpi4jax/experimental/__init__.py
@@ -1,3 +1,12 @@
+import jax
+from mpi4jax._src.jax_compat import versiontuple
+
+if versiontuple(jax.__version__) >= versiontuple("0.4.4"):
+    raise ImportError(
+        "mpi4jax.experimental is not yet compatible with JAX versions >= 0.4.4. "
+        "Please use jax<=0.4.3 for now."
+    )
+
 from .tokenizer import auto_tokenize
 
 __all__ = [
