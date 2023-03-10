ac55b2f4ef5eda4e761a0c9a6441599200a292a0
Roman Nuterman <nutrik@gmail.com>
2023-03-10 12:45:13 +0100

    fix decorators for cuda & hip mpi setup

--- mpi4jax/_src/decorators.py
+++ mpi4jax/_src/decorators.py
@@ -49,9 +49,12 @@ def setup_cuda_mpi():
         )
         warnings.warn(warn_msg)
 
-    from .xla_bridge import mpi_xla_bridge_gpu_cuda
+    try:
+        from .xla_bridge import mpi_xla_bridge_gpu_cuda
 
-    mpi_xla_bridge_gpu_cuda.set_copy_to_host(not has_cuda_mpi)
+        mpi_xla_bridge_gpu_cuda.set_copy_to_host(not has_cuda_mpi)
+    except ImportError:
+        warnings.warn("CUDA xla_bridge is not compiled")
 
 
 def setup_hip_mpi():
@@ -78,9 +81,12 @@ def setup_hip_mpi():
         )
         warnings.warn(warn_msg)
 
-    from .xla_bridge import mpi_xla_bridge_gpu_hip
+    try:
+        from .xla_bridge import mpi_xla_bridge_gpu_hip
 
-    mpi_xla_bridge_gpu_hip.set_copy_to_host(not has_hip_mpi)
+        mpi_xla_bridge_gpu_hip.set_copy_to_host(not has_hip_mpi)
+    except ImportError:
+        warnings.warn("HIP xla_bridge is not compiled")
 
 
 def translation_rule_cpu(func):
