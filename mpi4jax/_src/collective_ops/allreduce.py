2bd7adb109075a6c722a8099c71e40d80d23ffa4
Dion Häfner <dion.haefner@simulation.science>
2023-10-04 09:59:07 +0200

    fix gpu translation rules that still use out_types keyword (#216)

--- mpi4jax/_src/collective_ops/allreduce.py
+++ mpi4jax/_src/collective_ops/allreduce.py
@@ -156,13 +156,13 @@ def mpi_allreduce_xla_encode_gpu(ctx, x, token, op, comm, transpose):
 
     return hlo_custom_call(
         b"mpi_allreduce",
-        out_types=out_types,
+        result_types=out_types,
         operands=operands,
         operand_layouts=get_default_layouts(operands),
         result_layouts=get_default_layouts(out_types),
         has_side_effect=True,
         backend_config=descriptor,
-    )
+    ).results
 
 
 # This function evaluates only the shapes during AST construction
