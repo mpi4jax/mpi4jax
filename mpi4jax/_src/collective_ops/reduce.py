345d4dae5cb26a7569b19ba9bde3c48f5eca2395
Jacek Czaja <jacek.czaja@intel.com>
2024-01-23 16:15:42 +0100

    - Added missing code

--- mpi4jax/_src/collective_ops/reduce.py
+++ mpi4jax/_src/collective_ops/reduce.py
@@ -130,8 +130,57 @@ def mpi_reduce_xla_encode_cpu(ctx, x, token, op, root, comm):
 
 @translation_rule_xpu
 def mpi_reduce_xla_encode_xpu(ctx, x, token, op, root, comm):
-    print("XPU REDUCE not implemented!")
-    exit(-1)
+    from ..xla_bridge.mpi_xla_bridge_xpu import build_reduce_descriptor
+
+    op = unpack_hashable(op)
+    comm = unpack_hashable(comm)
+
+    x_aval, *_ = ctx.avals_in
+    x_nptype = x_aval.dtype
+
+    x_type = ir.RankedTensorType(x.type)
+    dtype = x_type.element_type
+    dims = x_type.shape
+
+    # compute total number of elements in array
+    nitems = _np.prod(dims, dtype=int)
+
+    dtype_handle = to_dtype_handle(x_nptype)
+
+    # output is only used on root, so prevent memory allocation
+    rank = comm.Get_rank()
+
+    if rank != root:
+        dims = (0,)
+
+    out_types = [
+        ir.RankedTensorType.get(dims, dtype),
+        *token_type(),
+    ]
+
+    operands = (
+        x,
+        token,
+    )
+
+    descriptor = build_reduce_descriptor(
+        nitems,
+        to_mpi_handle(op),
+        root,
+        to_mpi_handle(comm),
+        dtype_handle,
+    )
+
+    return custom_call(
+        b"mpi_reduce",
+        result_types=out_types,
+        operands=operands,
+        operand_layouts=get_default_layouts(operands),
+        result_layouts=get_default_layouts(out_types),
+        has_side_effect=True,
+        backend_config=descriptor,
+    ).results
+
 
 @translation_rule_gpu
 def mpi_reduce_xla_encode_gpu(ctx, x, token, op, root, comm):
