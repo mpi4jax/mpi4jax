4788a3fe8b0175aa801d63e038d2f007a0c35aa4
Sylwester Fraczek <sylwester.fraczek@intel.com>
2024-02-19 09:30:04 -0800

    add scan

--- mpi4jax/_src/collective_ops/scan.py
+++ mpi4jax/_src/collective_ops/scan.py
@@ -21,7 +21,11 @@ from ..utils import (
     prefer_notoken,
 )
 from ..jax_compat import custom_call, token_type, ShapedArray
-from ..decorators import translation_rule_cpu, translation_rule_gpu, translation_rule_xpu
+from ..decorators import (
+    translation_rule_cpu,
+    translation_rule_gpu,
+    translation_rule_xpu,
+)
 from ..validation import enforce_types
 from ..comm import get_default_comm
 
@@ -111,10 +115,8 @@ def mpi_scan_xla_encode_cpu(ctx, x, token, op, comm):
         has_side_effect=True,
     ).results
 
-@translation_rule_xpu
-def mpi_scan_xla_encode_xpu(ctx, x, token, op, comm):
-    from ..xla_bridge.mpi_xla_bridge_xpu import build_scan_descriptor
 
+def mpi_scan_xla_encode_device(ctx, x, token, op, comm, build_scan_descriptor):
     op = unpack_hashable(op)
     comm = unpack_hashable(comm)
 
@@ -156,52 +158,20 @@ def mpi_scan_xla_encode_xpu(ctx, x, token, op, comm):
         has_side_effect=True,
         backend_config=descriptor,
     ).results
+
+
+@translation_rule_xpu
+def mpi_scan_xla_encode_xpu(ctx, x, token, op, comm):
+    from ..xla_bridge.mpi_xla_bridge_xpu import build_scan_descriptor
+
+    return mpi_scan_xla_encode_device(ctx, x, token, op, comm, build_scan_descriptor)
+
 
 @translation_rule_gpu
 def mpi_scan_xla_encode_gpu(ctx, x, token, op, comm):
     from ..xla_bridge.mpi_xla_bridge_gpu import build_scan_descriptor
 
-    op = unpack_hashable(op)
-    comm = unpack_hashable(comm)
-
-    x_aval, *_ = ctx.avals_in
-    x_nptype = x_aval.dtype
-
-    x_type = ir.RankedTensorType(x.type)
-    dtype = x_type.element_type
-    dims = x_type.shape
-
-    # compute total number of elements in array
-    nitems = _np.prod(dims, dtype=int)
-
-    dtype_handle = to_dtype_handle(x_nptype)
-
-    out_types = [
-        ir.RankedTensorType.get(dims, dtype),
-        *token_type(),
-    ]
-
-    operands = (
-        x,
-        token,
-    )
-
-    descriptor = build_scan_descriptor(
-        nitems,
-        to_mpi_handle(op),
-        to_mpi_handle(comm),
-        dtype_handle,
-    )
-
-    return custom_call(
-        b"mpi_scan",
-        result_types=out_types,
-        operands=operands,
-        operand_layouts=get_default_layouts(operands),
-        result_layouts=get_default_layouts(out_types),
-        has_side_effect=True,
-        backend_config=descriptor,
-    ).results
+    return mpi_scan_xla_encode_device(ctx, x, token, op, comm, build_scan_descriptor)
 
 
 # This function evaluates only the shapes during AST construction
