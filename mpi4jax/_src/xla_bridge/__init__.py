cd9b0f0976e600ab49d08ac865a000d1d30c12b4
Filippo Vicentini <filippovicentini@gmail.com>
2024-06-11 10:53:30 +0200

    rename primitives

--- mpi4jax/_src/xla_bridge/__init__.py
+++ mpi4jax/_src/xla_bridge/__init__.py
@@ -19,11 +19,11 @@ else:
     HAS_XPU_EXT = True
 
 try:
-    from . import mpi_xla_bridge_gpu_hip
+    from . import mpi_xla_bridge_hip
 except ImportError:
-    HAS_GPU_HIP_EXT = False
+    HAS_HIP_EXT = False
 else:
-    HAS_GPU_HIP_EXT = True
+    HAS_HIP_EXT = True
 
 
 # setup logging
@@ -48,6 +48,6 @@ if HAS_XPU_EXT:
     for name, fn in mpi_xla_bridge_xpu.custom_call_targets.items():
         xla_client.register_custom_call_target(name, fn, platform="SYCL")
 
-if HAS_GPU_HIP_EXT:
-    for name, fn in mpi_xla_bridge_gpu_hip.gpu_custom_call_targets.items():
+if HAS_HIP_EXT:
+    for name, fn in mpi_xla_bridge_hip.custom_call_targets.items():
         xla_client.register_custom_call_target(name, fn, platform="ROCM")
